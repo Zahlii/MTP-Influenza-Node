35705da53933548efba03fdfa0a1a0fac231b640
Zahlii <niklasfruehauf@gmail.com>
2016-04-28 16:37:35 +0200

    Routing für heatmap aktualisiert: Now und History

--- controllers/heatmap.js
+++ controllers/heatmap.js
@@ -1,8 +1,12 @@
 var express = require('express'),
     router = express.Router();
 
-router.get('/:lat/:lng', function (req, res) {
+router.get('/now/:lat/:lng', function (req, res) {
     res.send('LatLng ' + req.params.lat + ', '+req.params.lng);
+});
+router.get('/history/:ts/:lat/:lng', function (req, res) {
+    var d = new Date(req.params.ts);
+    res.send(d.toLocaleString() + ' - LatLng ' + req.params.lat + ', '+req.params.lng);
 });
 
 
