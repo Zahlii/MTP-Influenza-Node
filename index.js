7a8ec53c1dacf3e747550f3480c355acb8a66cb1
Zahlii <niklasfruehauf@gmail.com>
2016-05-04 11:37:49 +0200

    Eingehende HTTP-Abfragen auf stdout loggen

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 var express = require('express'),
     port = process.env.PORT || 80,
 	bodyParser = require('body-parser'),
+	compression = require('compression');
     app = express();
 
 global.API_VERSION = 1;
 
 
+app.use(function(req, res, next){
+	var start = new Date;
+
+	res.on('finish', function(){
+		var duration = new Date - start;
+		console.log("[" +start.toJSON() + "] " + req.connection.remoteAddress + " | " + req.method + " " + req.originalUrl +" | "+ res.statusCode +" | " +duration+"ms");
+	});
+    next();
+});
+
 // Set up view engine
 app.set('views', __dirname + '/views');
 app.engine('jade', require('jade').__express);
 app.set('view engine', 'jade');
+
+app.use(compression());
 
 // Set up static server folder
 app.use(express.static(__dirname + '/www'));
