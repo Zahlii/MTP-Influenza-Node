32763b6f58fa867f7eb367e9cf6725a597543092
Zahlii <niklasfruehauf@gmail.com>
2016-05-09 12:34:08 +0200

    Heatmap-Funktionalität wieder auf /heatmap/* verlegt

--- routes/api1.js
+++ routes/api1.js
@@ -3,7 +3,7 @@ const Router = require('restify-router').Router;
 
 module.exports = (server) => {
     const api1router = new Router();
-    api1router.get('/now/:lat/:lng', (req, res, next) => {
+    api1router.get('/heatmap/now/:lat/:lng', (req, res, next) => {
         require('../controllers/heatmap')
             .displayHeatmapData(req.params.lat,req.params.lng,new Date(),res);
         return next();
@@ -15,7 +15,7 @@ module.exports = (server) => {
         return next();
     });
 
-    api1router.get('/history/:date/:lat/:lng', (req, res, next) => {
+    api1router.get('/heatmap/history/:date/:lat/:lng', (req, res, next) => {
         require('../controllers/heatmap')
             .displayHeatmapData(req.params.lat,req.params.lng,new Date(req.params.date),res)
         return next();
