cf6f3a0dc93c3c362b35f8fe6672dd9aed7086e3
Zahlii <niklasfruehauf@gmail.com>
2016-06-22 11:26:03 +0200

    MongoDB-Test

---
+++ mongo_test.js
@@ -0,0 +1,15 @@
+var MongoClient = require('mongodb').MongoClient;
+
+// Connection URL
+var url = 'mongodb://localhost:27017/mtp-influenza';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function (err, db) {
+    if (err != null) {
+        console.log('Error connecting to MongoDB: ' + err.message);
+    } else {
+        console.log('Connected successfully to MongoDB');
+    }
+    db.version();
+    db.close();
+});
