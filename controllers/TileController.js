bce0b35a59cdc9d590d91c6c891e4bb8cfb612bf
Zahlii <niklasfruehauf@gmail.com>
2016-09-28 20:38:29 +0200

    tiles

--- controllers/TileController.js
+++ controllers/TileController.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 const path = require('path');
 const base = path.resolve('./tiles');
 const heatmap = require('../util/heatmap');
+const SphericalMercator = require('sphericalmercator');
+const merc = new SphericalMercator({
+    size: 256
+});
 
 module.exports.renderTile = (req,res,next) => {
     var d = new Date(),
@@ -17,15 +21,26 @@ module.exports.renderTile = (req,res,next) => {
         renderTileInternal(x,y,z,i.path);
     }
     var img = fs.readFileSync(i.path);
-    res.writeHead(200, {'Content-Type': 'image/png' });
+
+    res.writeHead(200, {'Content-Type': 'image/png','Cache-Control': 'max-age=31556926' });
     res.end(img, 'binary');
 
     return next();
 };
 
+function getLocationCoordinates(x,y,z,lat,lng) {
+    var box = merc.bbox(x,y,z);
+    console.log(box);
+    // [ -168.75, 70.61261423801925, -157.5, 74.01954331150228 ] ne, sw
+
+}
 
 function renderTileInternal(x,y,z,path) {
-    //fs.touch
+    var heat = heatmap(256,256, { radius : 30 });
+
+    heat.draw();
+
+    fs.writeFileSync(path, heat.canvas.toBuffer());
 }
 
 function getTileInfo(x,y,z) {
