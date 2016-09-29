574e9d612bf857d7e1c1a4f610b4dd8169e62cbd
Influenza <influenza@vm-debian.bs-share.net>
2016-09-29 10:48:40 +0200

    Many performance improvements

--- controllers/TileController.js
+++ controllers/TileController.js
@@ -8,10 +8,10 @@ const SphericalMercator = require('sphericalmercator');
 const SIZE = 256;
 const mongoose = require('mongoose');
 const Location = mongoose.model('Location');
+const timing = require('../util/timing.js');
 
-const PERC = 20.0/SIZE;
-const PERC_MIN = 1-PERC;
-const PERC_MAX = 1+PERC;
+const PERC = 30.0/SIZE;
+
 
 
 const merc = new SphericalMercator({
@@ -19,6 +19,7 @@ const merc = new SphericalMercator({
 });
 
 module.exports.renderTile = (req,res,next) => {
+    timing.start(req);
     var d = new Date(),
         x = req.params.x,
         z = req.params.z,
@@ -26,15 +27,18 @@ module.exports.renderTile = (req,res,next) => {
 
     var i = getTileInfo(x,y,z),
         cb = (path) => {
-            var img = fs.readFileSync(path);
+            timing.elapsed('Sending file to client');
+            var img = fs.createReadStream(path);
 
-            res.writeHead(200, {'Content-Type': 'image/png','Cache-Control': 'max-age=31556926' });
-            res.end(img, 'binary');
-
+            //res.writeHead(200, {'Content-Type': 'image/png','Cache-Control': 'max-age=31556926' });
+            res.statusCode = 200;
+            img.pipe(res);
+            timing.elapsed('Sent file to client');
             return next();
         };
 
     if(!i.exists) {
+        timing.elapsed('Not existing, rendering...');
         renderTileInternal(x,y,z,i.path, cb);
     } else {
         cb(i.path);
@@ -56,17 +60,28 @@ function getLocationCoordinates(x,y,z,lat,lng) {
 }
 
 function getDataPoints(bbox,cb) {
-    // [ -168.75, 70.61261423801925, -157.5, 74.01954331150228 ] sw,ne
+    // [ 8.4375, 49.15296965617039, 8.7890625, 49.38237278700955 ] sw, ne
 
-    var q = [[bbox[2]*PERC_MIN,bbox[3]*PERC_MIN],
-        [bbox[0]*PERC_MAX,bbox[1]*PERC_MAX]];
 
-    //console.log(bbox,q);
+    // Add size to the box so that edges render correctly
+    var outlet = Math.abs(bbox[2]-bbox[0])*PERC;
+    bbox = [bbox[0]-outlet,bbox[1]-outlet,bbox[2]+outlet,bbox[3]+outlet];
 
+
+    var s = [[[bbox[0],bbox[1]],[bbox[0],bbox[3]],[bbox[2],bbox[3]],[bbox[2],bbox[1]],[bbox[0],bbox[1]]]];
+    //console.log(s);
+
+
+    // use 2DSphere
+    // {geo: { $geoWithin: { $geometry: { type:"Polygon", coordinates: [[[8,49],[8,50],[9,50],[9,49],[8,49]]] } } } }
+    // sw, nw, ne, se, sw
     Location.find({
         geo: {
             $geoWithin: {
-                $box: q
+                $geometry: {
+                    type:"Polygon",
+                    coordinates: s
+                }
             }
         }
     },cb);
@@ -80,12 +95,14 @@ function renderTileInternal(x,y,z,path,cb) {
     var box = merc.bbox(x,y,z);
 
     getDataPoints(box,(err,data) => {
+        timing.elapsed('Got required data points');
         var heat = heatmap(256,256, { radius : 30 });
 
         if(err) {
             //log.backgroundError("Failed loading heatmap tile data",err);
             path = base + '/tiles/clear.png';
         } else {
+            //console.log('Size: '+data.length);
             for (var i = 0, l = data.length; i < l; i++) {
                 var d = data[i];
                 var cx = getLocationCoordinates(x, y, z, d.geo.coordinates[1],d.geo.coordinates[0]);
--- model/index.js
+++ model/index.js
@@ -12,10 +12,11 @@ module.exports.initMongoose = () => {
 
         var h = mongoconfig.host;
 
-        if(os.hostname() != "wifo1-30")
+        if(os.hostname() != "wifo1-30" && os.hostname() != "vm-debian")
             h = "mongodb://wifo1-30.bwl.uni-mannheim.de:27016/mtp-influenza";
 
         mongoose.connect(h, mongoconfig.options);
+        console.log('Connected to '+h);
         fs.readdirSync('./model').forEach((file) => {
             if (file.substr(-3, 3) === '.js' && file !== 'index.js') {
                 const classname = file.replace('.js', '');
