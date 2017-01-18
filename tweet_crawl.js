eb68bf2e96a5c2ac2cd71b3d0cd4d3a55d923dbf
Zahlii <niklasfruehauf@gmail.com>
2017-01-18 17:59:21 +0100

    Enhanced tweet logging

--- tweet_crawl.js
+++ tweet_crawl.js
@@ -18,23 +18,36 @@ var client = new Twitter({
 
 const search = 'flu|influenza|grippe|sick|fever|coughing|cold|fieber|erkältung|husten|krank'.split('|');
 
+
+
 client.stream('statuses/filter', {locations:'-180,-90,180,90'}, function(stream) {
     stream.on('data', function(event) {
         if(event.text) {
             var lng = 0, lat = 0, pos = null, accuracy = -1;
             //console.log(event);
 
+            //console.log(event);
+
             if(event.place) {
                 var corners = event.place.bounding_box.coordinates[0];
 
+
+                var lngMin = Math.min(corners[0][0],corners[1][0],corners[2][0],corners[3][0]);
+                var lngMax = Math.max(corners[0][0],corners[1][0],corners[2][0],corners[3][0]);
+
+                var latMin = Math.min(corners[0][1],corners[1][1],corners[2][1],corners[3][1]);
+                var latMax = Math.max(corners[0][1],corners[1][1],corners[2][1],corners[3][1]);
+
+                var ne = new GeoPoint(latMin,lngMax);
+                var sw = new GeoPoint(latMax,lngMin);
+
                 pos = 'place';
 
-                var d1 = new GeoPoint(corners[0][1],corners[0][0]);
-                var d2 =  new GeoPoint(corners[2][1],corners[2][0]);
-                accuracy = d1.distanceTo(d2,true)/2;
+                accuracy = ne.distanceTo(sw,true)/2;
 
-                lng = (corners[0][0]+corners[2][0])/2;
-                lat = (corners[0][1]+corners[1][1])/2;
+                lng = lngMin + Math.random() * (lngMax - lngMin);
+                lat = latMin + Math.random() * (latMax - latMin);
+
             } else {
                 pos = 'geo';
                 accuracy = 0;
@@ -42,12 +55,24 @@ client.stream('statuses/filter', {locations:'-180,-90,180,90'}, function(stream)
                 lat = event.geo.coordinates[1];
             }
 
-            var data = {position: pos, lat:lat,lng:lng,accuracy: accuracy, text:event.text, time: (new Date()).toISOString()};
-            //console.log(data);
+            var data = {
+                position: pos,
+                lat:lat,
+                lng:lng,
+                accuracy: accuracy,
+                text:event.text,
+                time: (new Date()),
+                userId: event.user.id,
+                userName: event.user.screen_name,
+                tags: event.entities.hashtags,
+                symbols: event.entities.symbols,
+                language: event.user.lang
+            };
+
 
             var regex = new RegExp('(\\b' + search.join('\\b|\\b')+'\\b)','i');
             if(event.text.match(regex)) {
-                console.log(data);
+                console.log(JSON.stringify(data)+",");
             }
         }
     });
